82f97bcb50d21b8b516d308d91b58a6c75419eda
vu <vu@nxc.no>
2015-05-26 17:35:33 +0300

    first

--- javascript/main.js
+++ javascript/main.js
@@ -59,28 +59,112 @@
       }
     },
     _goLeft = function(){
-      var r, c = 3, isRowEnded = false;
+      var r, c = 3, isRowEnded = false, isUpdated = false;
       for (r = 0; r < 4; r++) {
         while (!isRowEnded) {
           if (map[r * 4 + c] != 0) {
             if (map[r * 4 + c - 1] == 0) {
               _merge(r * 4 + c, r * 4 + c - 1, false);
+              isUpdated = true;
             } else if (map[r * 4 + c - 1] == map[r * 4 + c]) {
               _merge(r * 4 + c, r * 4 + c - 1, true);
+              isUpdated = true;
             }
+            c--;
+          } else {
+            c--;
           }
-          if (c == -1) {
+          if (c <= 0) {
             isRowEnded = true;
           }
         }
-        c = 1;
+        isRowEnded = false;
+        c = 3;
       }
-      console.log(map);
-      _updateMirror();
+      return isUpdated;
     },
-    _merge = function(from, to, merge){
+    _goRight = function(){
+      var r, c = 0, isRowEnded = false, isUpdated = false;
+      for (r = 0; r < 4; r++) {
+        while (!isRowEnded) {
+          if (map[r * 4 + c] != 0) {
+            if (map[r * 4 + c + 1] == 0) {
+              _merge(r * 4 + c, r * 4 + c + 1, false);
+              isUpdated = true;
+            } else if (map[r * 4 + c + 1] == map[r * 4 + c]) {
+              _merge(r * 4 + c, r * 4 + c + 1, true);
+              isUpdated = true;
+            }
+            c++;
+          } else {
+            c++;
+          }
+          if (c >= 3) {
+            isRowEnded = true;
+          }
+        }
+        isRowEnded = false;
+        c = 0;
+      }
+      return isUpdated;
+    },
+    _goUp = function(){
+      var r = 3, c, isRowEnded = false, isUpdated = false;
+      for (c = 0; c < 4; c++) {
+        while (!isRowEnded) {
+          if (map[r * 4 + c] != 0) {
+            if (map[(r - 1) * 4 + c] == 0) {
+              _merge(r * 4 + c, (r - 1) * 4 + c, false);
+              isUpdated = true;
+            } else if (map[(r - 1) * 4 + c] == map[r * 4 + c]) {
+              _merge(r * 4 + c, (r - 1) * 4 + c, true);
+              isUpdated = true;
+            }
+            r--;
+          } else {
+            r--;
+          }
+          if (r <= 0) {
+            isRowEnded = true;
+          }
+        }
+        isRowEnded = false;
+        r = 3;
+      }
+      return isUpdated;
+    },
+    _goDown = function(){
+      var r = 0, c, isRowEnded = false, isUpdated = false;
+      for (c = 0; c < 4; c++) {
+        while (!isRowEnded) {
+          if (map[r * 4 + c] != 0) {
+            if (map[(r + 1) * 4 + c] == 0) {
+              _merge(r * 4 + c, (r + 1) * 4 + c, false);
+              isUpdated = true;
+            } else if (map[(r + 1) * 4 + c] == map[r * 4 + c]) {
+              _merge(r * 4 + c, (r + 1) * 4 + c, true);
+              isUpdated = true;
+            }
+            r++;
+          } else {
+            r++;
+          }
+          if (r >= 3) {
+            isRowEnded = true;
+          }
+        }
+        isRowEnded = false;
+        r = 0;
+      }
+      return isUpdated;
+    },
+    _removeNumber = function(element) {
+      element.parentNode.removeChild(element);
+    },
+    _merge = function(from, to, merge) {
       if (merge) {
         map[to] = map[from] + map[to];
+        _removeNumber(mapMirror[to]);
       } else {
         map[to] = map[from];
       }
@@ -89,11 +173,39 @@
       mapMirror[from] = 0;
     },
     _move = function(x, y) {
-      console.log(x, y);
+      var isUpdated, needsUpdate = true, i = 0;
+      while (needsUpdate) {
+        if (x == 0 && y == -1) {
+          needsUpdate = _goLeft();
+          if (i == 0) {
+            isUpdated = needsUpdate;
+          }
+        } else if (x == 0 && y == 1) {
+          needsUpdate = _goRight();
+          if (i == 0) {
+            isUpdated = needsUpdate;
+          }
+        } else if (x == 1 && y == 0) {
+          needsUpdate = _goUp();
+          if (i == 0) {
+            isUpdated = needsUpdate;
+          }
+        } else if (x == -1 && y == 0) {
+          needsUpdate = _goDown();
+          if (i == 0) {
+            isUpdated = needsUpdate;
+          }
+        }
+        i++;
+      }
+      if (isUpdated) {
+        _updateMirror();
+        _addRandom();
+      }
     },
     _keypress = function( event ) {
       if (event.keyCode == 37) {
-        _goLeft();
+        _move(0, -1);
       } else if (event.keyCode == 38) {
         _move(1, 0);
       } else if (event.keyCode == 39) {
